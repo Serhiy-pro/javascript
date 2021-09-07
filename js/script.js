3e9e8ee92a86467c26b2ee015a7cdd12ba94b868
Serhiy <polupansrhiy@gmail.com>
2021-09-07 23:10:43 +0300

    second home work, lesson_15

--- js/script.js
+++ js/script.js
@@ -9,12 +9,86 @@ const personalMovieDb = {
     privat: false
 };
 
-const a = prompt ('Один из последних просмотренных фильмов?', ''),
-      b = prompt ('На сколько оцените его?',''),
-      c = prompt ('Один из последних просмотренных фильмов?', ''),
-      d = prompt ('На сколько оцените его?','');
+for (let i = 0; i < 2; i++){
+    const a = prompt ('Один из последних просмотренных фильмов?', ''),
+          b = prompt ('На сколько оцените его?','');
 
-personalMovieDb.movie[a] = b;
-personalMovieDb.movie[c] = d;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+
+            personalMovieDb.movie[a] = b;
+        } else {
+            i--;
+        }
+}
+
+if (personalMovieDb.count <= 10){
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDb.count > 10 && personalMovieDb.count <= 30){
+    console.log('Вы класический зритель');
+} else if (personalMovieDb.count > 30){
+    console.log('Вы киноман');
+} else {
+    console.log('error');
+}
 
 console.log(personalMovieDb);
+
+
+
+// if (4 == 4) {
+//     console.log('ok');
+// } else {
+//     console.log('error');
+// }
+
+// const num = 50;
+
+// if (num < 50) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log('many');
+// } else {
+//     console.log('Ok');
+// }
+
+// const numb = 50;
+// (numb == 80) ? console.log('Ok') : console.log('error');
+
+// const number = 50;
+
+// switch (number) {
+//     case 49:
+//         console.log('error');
+//         break;
+//     case 51:
+//         console.log('error');
+//         break;
+//     case 52:
+//         console.log('yes');
+//     break;
+//     default:
+//         console.log('no');
+//     break;     
+// }
+
+
+
+// let newNum = 50;
+
+// while (newNum <= 60){
+//     console.log(newNum);
+//     newNum++;
+// }
+
+// let nu = 50;
+
+// do {
+//     console.log(nu);
+//     nu++;
+// }
+// while (nu < 60);
+
+// for (let i = 1; i < 8; i++){
+//     console.log(nu);
+//     nu++;
+// }
